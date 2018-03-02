a8c61a4d929703a70fa2f5fc5b3d4bcd6f9d05b0
arthurMargerit <arthur.margerit@edu.esiee.fr>
2018-03-02 22:32:11 +0100

    safe

--- src/main.py
+++ src/main.py
@@ -1,19 +1,19 @@
 import Config
-import template
+#import template
 import model_expand as model
 import sys
 
 
 def main():
     conf = Config.Configuration_manager()
-    jenv = template.load_jinja_env(conf)
+#    jenv = template.load_jinja_env(conf)
     data = model.file_expand(main, sys.argv[1])
-
-    template.generate_types(jenv, conf, data)
-    template.generate_structs(jenv, conf, data)
-    template.generate_interfaces(jenv, conf, data)
-    template.generate_composants(jenv, conf, data)
-    template.generate_deploiments(jenv, conf, data)
+    print(data)
+    # template.generate_types(jenv, conf, data)
+    # template.generate_structs(jenv, conf, data)
+    # template.generate_interfaces(jenv, conf, data)
+    # template.generate_composants(jenv, conf, data)
+    # template.generate_deploiments(jenv, conf, data)
 
 #    generate_composant(jenv,conf,dico);
 
