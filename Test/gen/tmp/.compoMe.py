31f6ccc65ebf2cb42557ca9ed04c1844a7f94ae2
tiregram <ruhtra.mar@gmail.com>
2019-08-07 22:56:35 +0200

    mise en place de .gitlabci

--- Test/gen/tmp/.compoMe.py
+++
@@ -1,5 +0,0 @@
-
-CONFIG = {
-    "jinja_template_path":"../../../generate_model/cpp",
-    "generation_model":"../../../generate_model/cpp/generation.yaml"
-}
--- Test/gen/tmp/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-set -euxo pipefail
-
-cmake . >> cmake.log
-make 2>&1  >> make.log
-./dep1 >> exec.log
-exit 0
