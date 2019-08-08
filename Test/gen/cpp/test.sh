4df635ce48531a81d59dc49fa025320e1a906175
tiregram <ruhtra.mar@gmail.com>
2019-08-08 23:59:27 +0200

    clean

--- Test/gen/cpp/test.sh
+++ Test/gen/cpp/test.sh
@@ -2,7 +2,11 @@
 
 set -euxo pipefail
 
-cmake . >> cmake.log
-make 2>&1  >> make.log
-./dep1 >> exec.log
+# cmake . >> cmake.log
+# make 2>&1  >> make.log
+# ./dep1 >> exec.log
+cmake .
+make
+./dep1
+
 exit 0
