99a140f97b65c09e0797fb7ab7115b6dcc7515b2
Arthur Margerit <arthur.margerit-ext@sagemcom.com>
2019-10-01 18:31:23 +0200

    (error) save: mise en place error interface rethrow

--- Test/gen/cpp/1.ERROR/1.simple/src/main.cpp
+++ Test/gen/cpp/1.ERROR/1.simple/src/main.cpp
@@ -1,16 +1,29 @@
 #include "Errors/Math_exeption.hpp"
+#include "Errors/Math_exeption_fac.hpp"
 #include "Errors/Division_exeption.hpp"
+#include "Data/code.hpp"
 #include <iostream>
+#include <sstream>
 
 int main(int argc, char *argv[])
 {
-  Math_exeption me;
-  Division_exeption de;
-  std::cout << me << std::endl;
-  std::cout << me.what() << std::endl << std::endl;
+  init_code();
 
-  std::cout << de << std::endl;
-  std::cout << de.what() << std::endl;
+  Math_exeption me;
+  // Division_exeption de;
+  // std::cout << me << std::endl;
+  // std::cout << me.what() << std::endl << std::endl;
+
+  // std::cout << de << std::endl;
+  // std::cout << de.what() << std::endl;
+
+
+  std::stringstream ss;
+  ss << me;
+  std::cout << me;
+  Error *me2 = NULL;
+  ss >> me2;
 
   return 0;
+
 }
