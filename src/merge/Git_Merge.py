3a988567b8e10c9e22108d910991dfed0f7fbdf3
tiregram <ruhtra.mar@gmail.com>
2019-11-16 19:48:28 +0100

    git merge

--- src/merge/Git_Merge.py
+++ src/merge/Git_Merge.py
@@ -1,6 +1,44 @@
 #!/usr/bin/env python3
 from merge.Merge import Merge
+import git
+
+
+def get_branch(p_c):
+    return p_c.name_rev.split(" ")[1].split("~")[0]
+
+
+def have_branch(repo, name):
+    return name in [b.name for b in repo.branches]
 
 
 class Git_Merge(Merge):
-    pass
+
+    def Create_arch_branch(i_branch):
+        pass
+
+    def __init__(self, p_opt, p_conf):
+        Merge.__init__(self, p_opt, p_conf)
+        self.repo = git.Repo(".")
+        i_branch = self.repo.head.ref.name
+
+        if "ARCHI" not in i_branch:
+            if not have_branch(self.repo, i_branch+"-ARCHI"):
+                self.repo.create_head("ARCHI")
+
+    def post(self):
+        Merge.post(self)
+        print("___ GIT POST ___")
+        self.repo.git.add(".")
+        self.repo.index.commit("ARCHI-"+self.prev_branch)
+        self.repo.git.checkout(self.prev_branch)
+        self.repo.git.merge("ARCHI")
+
+    def pre(self):
+        Merge.pre(self)
+        print("___ GIT PRE ___")
+        self.prev_branch = self.repo.head.ref.name
+        self.repo.git.checkout("ARCHI")
+
+    def report(self):
+        Merge.report(self)
+        print("___ GIT REPORT ___")
