ddaa8f066448d24ceb84340ffe00a8ad4c6213a2
tiregram <ruhtra.mar@gmail.com>
2019-11-18 20:53:54 +0100

    mise en place

---
+++ generate_model/cpp/Data/Struct_builder.cpp
@@ -0,0 +1,54 @@
+
+#include "Data/Struct_{{NAME}}_builder.hpp"
+#include "Data/Struct_fac_{{NAME}}.hpp"
+
+
+{{NAME}}_builder::{{NAME}}_builder(){%if PARENT%}:{{PARENT.NAME}}_builder(){%endif%} {}
+{{NAME}}_builder::~{{NAME}}_builder() {}
+
+
+{% if PARENT %}
+{% for i_d in  Function.model_get.get_all_field(PARENT.DATA,PARENT.PARENT) %}
+{{NAME}}_builder& {{NAME}}_builder::set_{{i_d.NAME}}(const {{i_d.TYPE.NAME}}& p_{{i_d.NAME}}){
+  {{PARENT.NAME}}_builder::set_{{i_d.NAME}}(p_{{i_d.NAME}});
+  return *this;
+}
+{% endfor %}
+{% endif %}
+
+{%for i_d in DATA %}
+{{NAME}}_builder& {{NAME}}_builder::set_{{i_d.NAME}}(const {{i_d.TYPE.NAME}}& p_{{i_d.NAME}}){
+    this->{{i_d.NAME}} = p_{{i_d.NAME}};
+    return *this;
+}
+{% endfor %}
+
+void {{NAME}}_builder::asign({{NAME}}& p_{{NAME}}) {
+  {% if PARENT %}
+  {{PARENT.NAME}}_builder::asign(p_{{NAME}});
+  {% endif %}
+
+  {%for i_d in DATA -%}
+  p_{{NAME}}.set_{{i_d.NAME}}(this->{{i_d.NAME}});
+  {% endfor %}
+}
+
+{{NAME}} {{NAME}}_builder::build() {
+    {{NAME}} p_this;
+    this->asign(p_this);
+    return p_this;
+}
+
+{{NAME}}* {{NAME}}_builder::build_p() {
+  {{NAME}}* p_this = new {{NAME}}();
+  this->asign(*p_this);
+  return p_this;
+}
+
+
+std::shared_ptr<{{NAME}}> {{NAME}}_builder::build_sp() {
+  auto p_this = std::make_shared<{{NAME}}>();
+  this->asign(*p_this);
+  return p_this;
+}
+
---
+++ generate_model/cpp/Data/Struct_builder.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Data/Struct_{{NAME}}.hpp"
+{% if PARENT %}
+#include "Data/Struct_{{PARENT.NAME}}_builder.hpp"
+{% endif %}
+#include <memory>
+
+class {{NAME}}_builder {%if PARENT%}: private {{PARENT.NAME}}_builder{% endif %}{
+
+ public:
+  {{NAME}}_builder();
+   virtual ~{{NAME}}_builder();
+
+ // setter
+   {%for i_d in Function.model_get.get_all_field(DATA,PARENT) %}
+
+  {{NAME}}_builder& set_{{i_d.NAME}}(const {{i_d.TYPE.NAME}}& p_{{i_d.NAME}});
+   {% endfor %}
+
+ // build
+   {{NAME}} build();
+   {{NAME}}* build_p();
+   std::shared_ptr<{{NAME}}> build_sp();
+
+
+ protected:
+   void asign({{NAME}}& p_name);
+
+ private:
+   {%for i_d in DATA %}
+ {{i_d.TYPE.NAME}} {{i_d.NAME}} {% if "DEFAULT" in i_d %}
+ = {{i_d.DEFAULT}}
+ {%elif "DEFAULT" in i_d.TYPE %}
+ = {{i_d.TYPE.DEFAULT}}
+ {% endif %};
+ {% endfor %}
+
+};
