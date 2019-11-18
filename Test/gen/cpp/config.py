18a8481a7109e97fbe57fe855bcac78b04cf5d91
tiregram <ruhtra.mar@gmail.com>
2019-11-18 22:51:40 +0100

    test

--- Test/gen/cpp/config.py
+++ Test/gen/cpp/config.py
@@ -1,7 +1,9 @@
 import os
 
 CONFIG = {
-    "jinja_template_path": [os.environ["COMPOME_PATH"]+"/generate_model/cpp","."],
-    "generation_model": os.environ["COMPOME_PATH"]+"/generate_model/cpp/generation.yaml",
-    "import_path": ["."]
+    "migration": "",
+    "jinja_template_path": [os.environ["COMPOME_PATH"] + "/generate_model/cpp", "."],
+    "generation_model": os.environ["COMPOME_PATH"] + "/generate_model/cpp/generation.yaml",
+    "import_path": ["."],
+    "template_options": {"project": {"name": "test"}}
 }
