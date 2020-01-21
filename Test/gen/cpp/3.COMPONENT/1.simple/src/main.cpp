b87a2ded74b9346dafc8078eb42b112af7865812
tiregram <ruhtra.mar@gmail.com>
2020-01-21 23:03:06 +0100

    correct clang

--- Test/gen/cpp/3.COMPONENT/1.simple/src/main.cpp
+++ Test/gen/cpp/3.COMPONENT/1.simple/src/main.cpp
@@ -1,121 +1,30 @@
-#include "Components/CA/CA.hpp"
-#include "Components/CB/CB.hpp"
-#include "Components/CC/CC.hpp"
-#include "Components/CD/CD.hpp"
-#include "Components/CE/CE.hpp"
-#include "Components/CF/CF.hpp"
 
-int main(int argc, char *argv[]) {
-  {
-    // ca
-    // ///////////////////////////////////////////////////////////////////////
-    CA::CA ca;
-    ca.configuration();
-    ca.connection();
+#include "Links/S_in/S_in.hpp"
+#include "Links/S_out/S_out.hpp"
 
-    ca.start();
-    int va = ca.get_a();
-    ca.set_a(va + 5);
-    ca.stop();
+#include "Components/C1/C1.hpp"
+#include "Components/C2/C2.hpp"
+
+int main() {
+
+  C1::C1 c1;
+  C2::C2 c2;
+
+  S_in sin;
+  S_out sout;
+
+  sin.set_in(&c1.get_ia());
+  sout.set_out(c2.fake_ia());
+
+  sin.connect();
+  sout.connect();
+
+  for (int i = 0; i < 100; ++i) {
+    sin.step();
+    sout.step();
   }
 
-  {
-    // cb
-    // ///////////////////////////////////////////////////////////////////////
-    CB::CB cb;
-    cb.configuration();
-    cb.connection();
-
-    cb.start();
-    int vb1 = cb.f1(1);
-    int vb2 = cb.f1(2);
-    cb.stop();
-  }
-
-  {
-    // cc
-    // ///////////////////////////////////////////////////////////////////////
-    CC::CC cc;
-    cc.configuration();
-    cc.connection();
-
-    cc.start();
-    cc.get_b();
-    cc.set_b(2);
-    cc.f1(1);
-    cc.get_p1().call1();
-    cc.set_r1(&cc.get_p1());
-    cc.stop();
-  }
-
-  {
-    // cd
-    // ///////////////////////////////////////////////////////////////////////
-    CD::CD cd;
-    cd.configuration();
-    cd.connection();
-
-    cd.start();
-    cd.get_p1().call1();
-    cd.get_p2().call1();
-    cd.get_p3().call1();
-    cd.get_p4().call1();
-
-    cd.stop();
-  }
-
-  {
-    // ce
-    // ///////////////////////////////////////////////////////////////////////
-    CE::CE ce;
-    CD::CD cd;
-
-    ce.configuration();
-    ce.connection();
-    cd.configuration();
-    cd.connection();
-
-    ce.start();
-    ce.set_r1(&cd.get_p1());
-    ce.set_r2(&cd.get_p1());
-    ce.set_r3(&cd.get_p1());
-    ce.set_r4(&cd.get_p1());
-    ce.stop();
-
-    // cf
-    // ///////////////////////////////////////////////////////////////////////
-    CF::CF cf;
-    cf.configuration();
-    cf.connection();
-
-    cf.start();
-    ce.set_r1(&cf.get_p1());
-    ce.set_r2(&cf.get_p2());
-    ce.set_r3(&cf.get_p3());
-    ce.set_r4(&cf.get_p4());
-    cf.stop();
-  }
-
-  {
-    // ce
-    // ///////////////////////////////////////////////////////////////////////
-    CE::CE ce;
-
-    ce.configuration();
-    ce.connection();
-    // Function_string_stream fs1,fs2,fs3,fs4;
-    // Return_string_stream rs1,rs2,rs3,rs4;
-
-    ce.start();
-    Fake* f1 = ce.fake_r1(// fs1,rs1
-                          );
-    Fake* f2 = ce.fake_r2(// fs2,rs2
-                          );
-    Fake* f3 = ce.fake_r3(// fs3,rs3
-                          );
-    Fake* f4 = ce.fake_r4(// fs4,rs4
-                          );
-    ce.stop();
-  }
+  sin.disconnect();
+  sout.disconnect();
   return 0;
 }
--- Test/gen/cpp/5.DEPLOYMENT/1.simple/src/main.cpp
+++ Test/gen/cpp/5.DEPLOYMENT/1.simple/src/main.cpp
@@ -1,9 +1,64 @@
+#include "Deployments/D0/D0.hpp"
+#include "Deployments/D0_c/D0_c.hpp"
+#include "Deployments/D0_l/D0_l.hpp"
+#include "Deployments/D1/D1.hpp"
+#include "Deployments/D1_c/D1_c.hpp"
+#include "Deployments/D1_l/D1_l.hpp"
+#include "Deployments/D2/D2.hpp"
+#include "Deployments/D2_c/D2_c.hpp"
+#include "Deployments/D3/D3.hpp"
+#include "Deployments/D3_c/D3_c.hpp"
+#include "Deployments/D4_c/D4_c.hpp"
+#include "Deployments/D_BIG/D_BIG.hpp"
 
 
 
+void extract(Deployment* d) {
 
-int main(int argc, char *argv[])
-{
+  d->init();
+  d->configuration();
+  d->link();
+
+  d->start();
+  for (int i = 0; i < 1000; ++i) {
+    d->step();
+  }
+  d->stop();
+
+  d->quit();
+}
+
+int main(int argc, char *argv[]) {
+  D0 da;
+  D0_c db;
+  D0_l dc;
+
+  D1 dd;
+  D1_c de;
+  D1_l df;
+
+  D2 dg;
+  D2_c dh;
+
+  D3 di;
+  D3_c dj;
+
+  D4_c dk;
+
+  D_BIG dl;
+
+  extract(&da);
+  extract(&db);
+  extract(&dc);
+  extract(&dd);
+  extract(&de);
+  extract(&df);
+  extract(&dg);
+  extract(&dh);
+  extract(&dj);
+  extract(&dk);
+  extract(&dl);
   
+
   return 0;
 }
