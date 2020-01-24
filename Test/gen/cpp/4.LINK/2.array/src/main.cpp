b18f602b5d23a9873b0d571bdd8a0b6908e7306e
tiregram <ruhtra.mar@gmail.com>
2020-01-24 19:03:56 +0100

    add link test

--- Test/gen/cpp/4.LINK/2.array/src/main.cpp
+++ Test/gen/cpp/4.LINK/2.array/src/main.cpp
@@ -1,8 +1,57 @@
 
+#include "Links/S_in/S_in.hpp"
+#include "Links/S_out/S_out.hpp"
 
+#include "Components/C1/C1.hpp"
+#include "Components/C2/C2.hpp"
+#include "Deployments/D/D.hpp"
 
-int main(int argc, char *argv[])
-{
-  
+void without_dep() {
+
+  C1::C1 c1;
+  C2::C2 c2;
+
+  S_in sin;
+  S_out sout;
+
+  sin.set_in(0, &c1.get_ia());
+  sout.set_out(0, c2.fake_ia());
+
+  sin.set_in(1, &c1.get_ia());
+  sout.set_out(1, c2.fake_ia());
+
+  sin.set_in(2, &c1.get_ia());
+  sout.set_out(2, c2.fake_ia());
+
+  sin.connect();
+  sout.connect();
+
+  for (int i = 0; i < 100; ++i) {
+    sin.step();
+    sout.step();
+  }
+
+  sin.disconnect();
+  sout.disconnect();
+}
+
+void with_dep() {
+  D d;
+  d.init();
+  d.configuration();
+  d.link();
+  d.start();
+
+  for (int i = 0; i < 100; ++i) {
+    d.step();
+  }
+
+  d.stop();
+  d.quit();
+}
+
+int main() {
+  without_dep();
+  with_dep();
   return 0;
 }
--- Test/gen/cpp/4.LINK/3.map/src/main.cpp
+++ Test/gen/cpp/4.LINK/3.map/src/main.cpp
@@ -1,8 +1,57 @@
 
+#include "Links/S_in/S_in.hpp"
+#include "Links/S_out/S_out.hpp"
 
+#include "Components/C1/C1.hpp"
+#include "Components/C2/C2.hpp"
+#include "Deployments/D/D.hpp"
 
-int main(int argc, char *argv[])
-{
-  
+void without_dep() {
+
+  C1::C1 c1;
+  C2::C2 c2;
+
+  S_in sin;
+  S_out sout;
+
+  sin.set_in("lapin", &c1.get_ia());
+  sout.set_out("lapin", c2.fake_ia());
+
+  sin.set_in("lion", &c1.get_ia());
+  sout.set_out("lion", c2.fake_ia());
+
+  sin.set_in("cochon", &c1.get_ia());
+  sout.set_out("cochon", c2.fake_ia());
+
+  sin.connect();
+  sout.connect();
+
+  for (int i = 0; i < 100; ++i) {
+    sin.step();
+    sout.step();
+  }
+
+  sin.disconnect();
+  sout.disconnect();
+}
+
+void with_dep() {
+  D d;
+  d.init();
+  d.configuration();
+  d.link();
+  d.start();
+
+  for (int i = 0; i < 100; ++i) {
+    d.step();
+  }
+
+  d.stop();
+  d.quit();
+}
+
+int main() {
+  without_dep();
+  with_dep();
   return 0;
 }
