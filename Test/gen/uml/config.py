362ff31d88cfd44ff9aca1e34131b04d1b1e27c0
tiregram <ruhtra.mar@gmail.com>
2020-02-12 22:53:40 +0100

    correct config

--- Test/gen/uml/config.py
+++ Test/gen/uml/config.py
@@ -1,6 +1,9 @@
 import os
 
 CONFIG = {
+    "migration": "simple",
     "jinja_template_path": os.environ["COMPOME_PATH"]+"/generate_model/uml",
-    "generation_model": os.environ["COMPOME_PATH"]+"/generate_model/uml/generation.yaml"
+    "generation_model": os.environ["COMPOME_PATH"]+"/generate_model/uml/generation.yaml",
+    "import_path": ["."],
+    "template_options": {"project": {"name": "test"}}
 }
