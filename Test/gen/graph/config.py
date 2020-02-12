940787c27cd3cbee68fb16947658aad7a0fd7133
tiregram <ruhtra.mar@gmail.com>
2020-02-12 22:22:04 +0100

    correct link

--- Test/gen/graph/config.py
+++ Test/gen/graph/config.py
@@ -1,6 +1,6 @@
 import os
 
 CONFIG = {
-    "jinja_template_path":[os.environ["COMPOME_PATH"]+"/generate_model/graph/","."],
+    "jinja_template_path": [os.environ["COMPOME_PATH"]+"/generate_model/graph/", "."],
     "generation_model": os.environ["COMPOME_PATH"]+"/generate_model/graph/generation.yaml"
 }
