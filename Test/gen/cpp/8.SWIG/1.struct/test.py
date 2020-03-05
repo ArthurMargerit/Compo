b559bbab3c92cdf05c38439c858cb7aa24ae922a
tiregram <ruhtra.mar@gmail.com>
2020-03-05 19:39:31 +0100

    swig correction

--- Test/gen/cpp/8.SWIG/1.struct/test.py
+++ Test/gen/cpp/8.SWIG/1.struct/test.py
@@ -4,7 +4,6 @@ import Struct_s1
 import Struct_s2
 import Struct_s3
 
-
 l_s0 = Struct_s0.s0()
 l_s1 = Struct_s1.s1()
 l_s2 = Struct_s2.s2()
@@ -25,9 +24,8 @@ del l_s1
 del l_s2
 del l_s3
 
-
 l_s0 = Struct_s0.s0()
-l_s1 = Struct_s1.s1(1,2,3)
+l_s1 = Struct_s1.s1(1, 2, 3)
 l_s2 = Struct_s2.s2(4)
 l_s3 = Struct_s3.s3(l_s0.this, l_s1.this, l_s2.this)
 
@@ -47,10 +45,11 @@ l_s1.get_d1()
 l_s1.get_d1()
 l_s1.get_d1()
 
-l_s0.to_stream()
-l_s1.to_stream()
-l_s2.to_stream()
-l_s3.to_stream()
+print(l_s0)
+print(l_s1)
+print(l_s2)
+print(l_s3)
+
 
 del l_s0
 del l_s1
--- generate_model/cpp/Data/Struct_swig.i
+++ generate_model/cpp/Data/Struct_swig.i
@@ -1,12 +1,17 @@
 /* {{NAME}}.i */
 
+%include <std_string.i>
+%feature("python:slot", "tp_str", functype="reprfunc") {{NAME}}::to_string;
+
 %rename(steam_me_in) operator>>;
+%rename(__str__) to_string;
 %rename(stream_me_out) operator<<;
 
 %module Struct_{{NAME}}
 %{
+#include "Data/Struct.hpp"
 #include "Data/Struct_{{NAME}}.hpp"
+#include "Serialization_context.hpp"
 %}
 
-%include "Data/Struct.hpp"
 %include "Data/Struct_{{NAME}}.hpp"
