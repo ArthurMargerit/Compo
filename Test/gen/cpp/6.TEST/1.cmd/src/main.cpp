8d8392136daa2f354733b4d8a167516026522bc3
tiregram <ruhtra.mar@gmail.com>
2020-04-20 11:32:13 +0200

    TERM + correction

--- Test/gen/cpp/6.TEST/1.cmd/src/main.cpp
+++ Test/gen/cpp/6.TEST/1.cmd/src/main.cpp
@@ -5,66 +5,66 @@
 
 #include <sstream>
 
-class Return_r_stream: public Return_stream {
-public:
-  Return_r_stream(std::ostream *os):Return_stream(os)
-  {}
-  Return_r_stream(std::istream *is):Return_stream((std::istream*)&a_ss),a_is(is)
-  {}
+// class Return_r_stream: public Return_stream {
+// public:
+//   Return_r_stream(std::ostream *os):Return_stream(os)
+//   {}
+//   Return_r_stream(std::istream *is):Return_stream((std::istream*)&a_ss),a_is(is)
+//   {}
 
-  std::istream *a_is;
-  std::stringstream a_ss;
+//   std::istream *a_is;
+//   std::stringstream a_ss;
 
-  virtual void pull() {
-    std::string s;
-    std::getline(*this->a_is,s);
-    this->a_ss << s;
-  }
+//   virtual void pull() {
+//     std::string s;
+//     std::getline(*this->a_is,s);
+//     this->a_ss << s;
+//   }
 
-  virtual void end() {}
-};
+//   virtual void end() {}
+// };
 
-class Function_r_stream: public Function_stream {
-public:
-  Function_r_stream(std::istream *is):Function_stream(is)
-  {}
-  Function_r_stream(std::ostream *os):Function_stream(os)
-  {}
-  virtual void start() {}
-  virtual void call() {}
+// class Function_r_stream: public Function_stream {
+// public:
+//   Function_r_stream(std::istream *is):Function_stream(is)
+//   {}
+//   Function_r_stream(std::ostream *os):Function_stream(os)
+//   {}
+//   virtual void start() {}
+//   virtual void call() {}
 
-};
+// };
 
 int main(int argc, char *argv[]) {
 
   init_code();
 
-  C1::C1 c1;
-  auto c = c1.get_p1().get_caller();
-  Function_r_stream fs(&std::cin);
-  Return_r_stream rs(&std::cout);
-  while (true){
-    std::cout << "\n>>>";
+  // C1::C1 c1;
+  // auto c = c1.get_p1().get_caller();
+  // Function_r_stream fs(&std::cin);
+  // Return_r_stream rs(&std::cout);
+  // while (true){
+  //   std::cout << "\n>>>";
 
-    // if closed
-    if(! std::cin.good()){
-      break;
-    }
+  //   // if closed
+  //   if(! std::cin.good()){
+  //     break;
+  //   }
 
-    // remove space
-    while(std::cin.peek() == ' '){
-      std::cin.get();
-    }
+  //   // remove space
+  //   while(std::cin.peek() == ' '){
+  //     std::cin.get();
+  //   }
 
-    // valid empty
-    if(std::cin.peek() == '\n'){
-      std::cin.get();
-      continue;
-    }
+  //   // valid empty
+  //   if(std::cin.peek() == '\n'){
+  //     std::cin.get();
+  //     continue;
+  //   }
 
-    // call
-    c->call(fs,rs);
-  }
+  //   // call
+  //   c->call(fs,rs);
+  // }
 
   return 0;
 }
