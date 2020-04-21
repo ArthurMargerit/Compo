86c54ab565d327f396f0d968ec420f77d14d4a01
tiregram <ruhtra.mar@gmail.com>
2020-04-21 18:29:42 +0200

    TERM + correction

--- Test/gen/cpp_base/before.sh
+++ Test/gen/cpp_base/before.sh
@@ -1,3 +1,12 @@
 
-export COMPOME_PATH="$(realpath ../../..)"
+if [[ -v COMPO_WORKDIR_TMP && COMPO_WORKDIR_TMP == 1 ]]
+then
+    export COMPOME_PATH="$(realpath ../../..)"
+else
+    p=$(pwd)
+    cd -
+    export COMPOME_PATH="$(realpath ../..)"
+    cd ${p}
+fi
+
 export COMPOME_MODEL_PATH=.
