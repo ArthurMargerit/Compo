a82d9cdb640921e97d4c8c06738ec1aef96a1d50
tiregram <ruhtra.mar@gmail.com>
2020-05-13 18:12:49 +0200

    add test

--- Test/gen/cpp/1.STRUCT/2.parent/src/main.cpp
+++ Test/gen/cpp/1.STRUCT/2.parent/src/main.cpp
@@ -13,131 +13,169 @@
 #include "Data/code.hpp"
 #include <sstream>
 
-TEST_CASE("Type Parent", "[Type][Parent]") {
+TEST_CASE("Struct Parent heritage get/set", "[Struct][Parent][Get/Set]") {
   init_code();
-  {
-    AAAAAA a;
-    a.set_a1(1);
-    a.set_a2(2);
-    a.set_a3(3);
-    a.set_a4(4);
-    a.set_a5(5);
-    a.set_a6(6);
+  AAAAAA a;
+  a.set_a1(1);
+  REQUIRE(a.get_a1() == 1);
+  REQUIRE(a.a_a1() == 1);
+  a.a_a1() = -1;
+  REQUIRE(a.a_a1() == -1);
 
-    AAAAAA *p1 = NULL;
-    AAAAA *p2 = NULL;
-    AAAA *p3 = NULL;
-    AAA *p4 = NULL;
-    AA *p5 = NULL;
-    A *p6 = NULL;
-    Struct *p7 = NULL;
+  a.set_a2(2);
+  REQUIRE(a.get_a2() == 2);
+  REQUIRE(a.a_a2() == 2);
+  a.a_a2() = -2;
+  REQUIRE(a.a_a2() == -2);
 
-    std::stringstream ss;
-    std::cout << &a;
-    ss << &a << &a << &a << &a << &a << &a << &a;
-    ss >> p1 >> p2 >> p3 >> p4 >> p5 >> p6 >> p7;
-    std::cout << p1 << std::endl;
-    std::cout << p2 << std::endl;
-    std::cout << p3 << std::endl;
-    std::cout << p4 << std::endl;
-    std::cout << p5 << std::endl;
-    std::cout << p6 << std::endl;
-    std::cout << p7 << std::endl;
-  }
-  {
-    AAAAA a;
-    a.set_a1(1);
-    a.set_a2(2);
-    a.set_a3(3);
-    a.set_a4(4);
-    a.set_a5(5);
+  a.set_a3(3);
+  REQUIRE(a.get_a3() == 3);
+  REQUIRE(a.a_a3() == 3);
+  a.a_a3() = -3;
+  REQUIRE(a.a_a3() == -3);
 
-    AAAAA *p2 = NULL;
-    AAAA *p3 = NULL;
-    AAA *p4 = NULL;
-    AA *p5 = NULL;
-    A *p6 = NULL;
-    Struct *p7 = NULL;
+  a.set_a4(4);
+  REQUIRE(a.get_a4() == 4);
+  REQUIRE(a.a_a4() == 4);
+  a.a_a4() = -4;
+  REQUIRE(a.a_a4() == -4);
 
-    std::stringstream ss;
-    std::cout << &a;
-    ss << &a << &a << &a << &a << &a << &a;
-    ss >> p2 >> p3 >> p4 >> p5 >> p6 >> p7;
-    std::cout << p2 << std::endl;
-    std::cout << p3 << std::endl;
-    std::cout << p4 << std::endl;
-    std::cout << p5 << std::endl;
-    std::cout << p6 << std::endl;
-    std::cout << p7 << std::endl;
-  }
-  {
-    AAAA a;
-    a.set_a1(1);
-    a.set_a2(2);
-    a.set_a3(3);
-    a.set_a4(4);
+  a.set_a5(5);
+  REQUIRE(a.get_a5() == 5);
+  REQUIRE(a.a_a5() == 5);
+  a.a_a5() = -5;
+  REQUIRE(a.a_a5() == -5);
 
-    AAAA *p3 = NULL;
-    AAA *p4 = NULL;
-    AA *p5 = NULL;
-    A *p6 = NULL;
-    Struct *p7 = NULL;
+  a.set_a6(6);
+  REQUIRE(a.get_a6() == 6);
+  REQUIRE(a.a_a6() == 6);
+  a.a_a5() = -6;
+  REQUIRE(a.a_a5() == -6);
+}
 
-    std::stringstream ss;
-    std::cout << &a;
-    ss << &a << &a << &a << &a << &a;
-    ss >> p3 >> p4 >> p5 >> p6 >> p7;
-    std::cout << p3 << std::endl;
-    std::cout << p4 << std::endl;
-    std::cout << p5 << std::endl;
-    std::cout << p6 << std::endl;
-    std::cout << p7 << std::endl;
-  }
-  {
-    AAA a;
-    a.set_a1(1);
-    a.set_a2(2);
-    a.set_a3(3);
+TEST_CASE("Struct Parent heritage ClassPoly", "[Struct][Parent][Poly]") {
+  AAAAAA a;
+  a.set_a1(1);
+  a.set_a2(2);
+  a.set_a3(3);
+  a.set_a4(4);
+  a.set_a5(5);
+  a.set_a6(6);
 
-    AAA *p4 = NULL;
-    AA *p5 = NULL;
-    A *p6 = NULL;
-    Struct *p7 = NULL;
+  AAAAAA *p1 = NULL;
+  AAAAA *p2 = NULL;
+  AAAA *p3 = NULL;
+  AAA *p4 = NULL;
+  AA *p5 = NULL;
+  A *p6 = NULL;
+  Struct *p7 = NULL;
 
-    std::stringstream ss;
-    std::cout << &a;
-    ss << &a << &a << &a << &a;
-    ss >> p4 >> p5 >> p6 >> p7;
-    std::cout << p4 << std::endl;
-    std::cout << p5 << std::endl;
-    std::cout << p6 << std::endl;
-    std::cout << p7 << std::endl;
-  }
-  {
-    AA a;
-    a.set_a1(1);
-    a.set_a2(2);
+  p1 = dynamic_cast<AAAAAA *>(&a);
+  REQUIRE(p1 != nullptr);
+  REQUIRE(p1->get_a6() == 6);
 
-    AA *p5 = NULL;
-    A *p6 = NULL;
-    Struct *p7 = NULL;
+  p2 = dynamic_cast<AAAAA *>(&a);
+  REQUIRE(p2 != nullptr);
+  REQUIRE(p2->get_a5() == 5);
 
-    std::stringstream ss;
-    std::cout << &a;
-    ss << &a << &a << &a;
-    ss >> p5 >> p6 >> p7;
-    std::cout << p5 << std::endl;
-    std::cout << p6 << std::endl;
-    std::cout << p7 << std::endl;
-  }
+  p3 = dynamic_cast<AAAA *>(&a);
+  REQUIRE(p3 != nullptr);
+  REQUIRE(p3->get_a4() == 4);
 
-  {
-    std::shared_ptr<Struct> s = std::make_shared<AAAAAA>();
-    std::shared_ptr<A> sa = std::make_shared<AAAAAA>();
-    std::shared_ptr<AA> saa = std::make_shared<AAAAAA>();
-    std::shared_ptr<AAA> saaa = std::make_shared<AAAAAA>();
-    std::shared_ptr<AAAA> saaaa = std::make_shared<AAAAAA>();
-    std::shared_ptr<AAAAA> saaaaa = std::make_shared<AAAAAA>();
-    std::shared_ptr<AAAAAA> saaaaaa = std::make_shared<AAAAAA>();
-  }
+  p4 = dynamic_cast<AAA *>(&a);
+  REQUIRE(p4 != nullptr);
+  REQUIRE(p4->get_a3() == 3);
+
+  p5 = dynamic_cast<AA *>(&a);
+  REQUIRE(p5 != nullptr);
+  REQUIRE(p5->get_a2() == 2);
+
+  p6 = dynamic_cast<A *>(&a);
+  REQUIRE(p6 != nullptr);
+  REQUIRE(p6->get_a1() == 1);
+
+  p7 = dynamic_cast<Struct *>(&a);
+  REQUIRE(p7 != nullptr);
+}
+
+TEST_CASE("Struct Parent Serialization",
+          "[Struct][Parent][Poly][Serialization]") {
+  std::stringstream ss;
+  AAAAAA a;
+  ss << a;
+  INFO("A: " << a);
+
+  auto p = ss.str().find("type:AAAAAA");
+  REQUIRE(p != std::string::npos);
+  p = ss.str().find("type:AAAAA");
+  REQUIRE(p != std::string::npos);
+  p = ss.str().find("type:AAAA");
+  REQUIRE(p != std::string::npos);
+  p = ss.str().find("type:AAA");
+  REQUIRE(p != std::string::npos);
+  p = ss.str().find("type:AA");
+  REQUIRE(p != std::string::npos);
+  p = ss.str().find("type:A");
+  REQUIRE(p != std::string::npos);
+}
+
+TEST_CASE("Struct Parent Serialization 2",
+          "[Struct][Parent][Poly][Serialization]") {
+
+  std::stringstream ss;
+  AAAAAA a_e, a_i;
+  a_e.set_a6(6);
+  a_e.set_a5(5);
+  a_e.set_a4(4);
+  a_e.set_a3(3);
+  a_e.set_a2(2);
+  a_e.set_a1(1);
+
+  ss << a_e;
+  ss >> a_i;
+
+  REQUIRE(a_i.get_a1() == 1);
+  REQUIRE(a_i.get_a2() == 2);
+  REQUIRE(a_i.get_a3() == 3);
+  REQUIRE(a_i.get_a4() == 4);
+  REQUIRE(a_i.get_a5() == 5);
+  REQUIRE(a_i.get_a6() == 6);
+}
+
+TEST_CASE("Struct Parent Serialization dynamic cast",
+          "[Struct][Parent][Poly][Serialization]") {
+
+  AAAAAA *a = new AAAAAA();
+  REQUIRE(a != NULL);
+
+  AAAAA *p1 = dynamic_cast<AAAAA *>(a);
+  REQUIRE(p1 != NULL);
+
+  AAAA *p2 = dynamic_cast<AAAA *>(a);
+  REQUIRE(p2 != NULL);
+
+  AAA *p3 = dynamic_cast<AAA *>(a);
+  REQUIRE(p3 != NULL);
+
+  AA *p4 = dynamic_cast<AA *>(a);
+  REQUIRE(p4 != NULL);
+
+  A *p5 = dynamic_cast<A *>(a);
+  REQUIRE(p5 != NULL);
+  delete (a);
+}
+
+TEST_CASE("Struct Parent Serialization cast", "[Struct][Parent][Poly][cast]") {
+
+  A *a = new A();
+  REQUIRE(a != NULL);
+
+  AAAAA *p1 = dynamic_cast<AAAAA *>(a);
+  REQUIRE(p1 == NULL);
+  AAAA *p2 = dynamic_cast<AAAA *>(a);
+  REQUIRE(p2 == NULL);
+  AAA *p3 = dynamic_cast<AAA *>(a);
+  REQUIRE(p3 == NULL);
+  AA *p4 = dynamic_cast<AA *>(a);
+  REQUIRE(p4 == NULL);
 }
--- Test/gen/cpp/1.STRUCT/3.complex/src/main.cpp
+++ Test/gen/cpp/1.STRUCT/3.complex/src/main.cpp
@@ -9,176 +9,200 @@
 #include <memory>
 #include <sstream>
 
-TEST_CASE("Struct complex", "[Struct][complex]"){
+TEST_CASE("Struct complex basic pointer", "[Struct][complex][Pointer]") {
   init_code();
 
-  { // b1
-    Base1 b1;
-    b1.a_ea().set_pa(99);
-    b1.a_ea().set_pb(99);
-    std::cout << b1 << std::endl;
+  Base1 b1, b2;
+  b1.a_ea().set_pa(99);
+  b1.a_ea().set_pb(99);
 
-    b1.set_ep(&b1.a_ea());
-    std::cout << b1 << std::endl;
+  std::stringstream ss1;
+  ss1 << b1;
+  REQUIRE(ss1.str().find("ep:*(0)") != std::string::npos); // found
+  ss1.str("");
 
-    Elem e;
-    b1.set_ep(&e);
-    std::cout << b1 << "\n";
-  }
+  b1.set_ep(&b1.a_ea());
+  ss1 << b1;
+  REQUIRE(ss1.str().find("ep:*(0)") == std::string::npos); // not found
 
-  {
-    // b1
-    Base1_rev b1;
-    b1.a_ea().set_pa(99);
-    b1.a_ea().set_pb(99);
-    std::cout << b1 << std::endl;
+  std::stringstream ss;
+  ss << b1;
+  ss >> b2;
 
-    b1.set_ep(&b1.a_ea());
-    std::cout << b1 << std::endl;
+  REQUIRE(b2.a_ea().get_pa() == 99);
+  REQUIRE(b2.a_ea().get_pb() == 99);
+  REQUIRE(b2.a_ep()->get_pa() == 99);
+  REQUIRE(b2.a_ep()->get_pb() == 99);
+  REQUIRE(&b2.a_ea() == b2.get_ep());
+  REQUIRE(&b2.a_ep() != &b1.a_ep());
 
-    Elem e;
-    b1.set_ep(&e);
-    std::cout << b1 << "\n";
-  }
+  Elem e;
+  e.set_pa(1);
+  e.set_pb(2);
+  b1.set_ep(&e);
 
-  {
-    // a* - a
-    pNode pa;
-    Node a;
-    pa = &a;
-    std::cout << a << "\n";
-    std::cout << pa << "\n";
-    std::cout << &pa << "\n";
-  }
+  ss << b1;
+  ss >> b2;
 
-  {
-    //       - b -
-    // a - <       > - d
-    //       - c -
-    Node a, b, c, d;
-    a.set_g(&b);
-    a.set_d(&c);
-    b.set_d(&d);
-    c.set_g(&d);
-    std::cout << a << "\n";
-    std::cout << b << "\n";
-    std::cout << c << "\n";
-    std::cout << d << "\n";
-
-    std::cout << &a << "\n";
-    std::cout << &b << "\n";
-    std::cout << &c << "\n";
-    std::cout << &d << "\n";
-
-    Node n;
-    std::stringstream ss;
-    ss << a;
-    ss >> n;
-
-    std::cout << "____"
-              << "\n";
-    std::cout << a << "\n";
-    std::cout << " == "
-              << "\n";
-    std::cout << n << "\n";
-    std::cout << "____"
-              << "\n";
-
-    if (n.a_d() == NULL) {
-      std::cerr << "Error 1";
-      throw "Error 1";
-    }
-
-    if (n.a_g() == NULL) {
-      std::cerr << "Error 2";
-      throw "Error 2";
-    }
-
-    if (n.a_g()->a_d() != n.a_d()->a_g()) {
-      std::cerr << "Error 3";
-      throw "Error 3";
-    }
-  }
-
-  {
-    Node a, b, c, d;
-    Node a1, b1, c1, d1;
-
-    a.a_g() = &b;
-    b.a_g() = &c;
-    c.a_g() = &d;
-    d.a_g() = &a;
-
-    std::cout << a << "\n";
-    std::cout << b << "\n";
-    std::cout << c << "\n";
-    std::cout << d << "\n";
-
-    std::stringstream ss;
-    ss << a << b << c << d;
-    ss >> a1 >> b1 >> c1 >> d1;
-  }
-
-  {
-    psNode ps;
-    if (ps != nullptr) {
-      std::cerr << "Erreur 4";
-      throw "erreur 5";
-    }
-
-    for (int i = 0; i < 100; ++i) {
-      ps = std::make_shared<Node2>();
-    }
-
-    ps = nullptr;
-  }
-
-  // node point to the same
-  {
-    Node2 n2a;
-    Node2 n2b;
-    n2a.g = std::make_shared<Node2>();
-    n2a.d = std::make_shared<Node2>();
-    auto n = std::make_shared<Node2>();
-    n2a.g->g = n;
-    n2a.g->d = n;
-    n2a.d->g = n;
-    n2a.d->d = n;
-
-    std::cout << n2a << "\n";
-    std::cout << n2b << "\n";
-
-    std::stringstream ss;
-    ss << n2a;
-    ss >> n2b;
-
-    std::cout << n2a << "\n";
-    std::cout << n2b << "\n";
-  }
-
-  // circular one
-  {
-    auto n2a1 = std::make_shared<Node2>();
-    auto n2a2 = std::make_shared<Node2>();
-    auto n2a3 = std::make_shared<Node2>();
-    auto n2a4 = std::make_shared<Node2>();
-    auto n2a5 = std::make_shared<Node2>();
-
-    n2a1->g = n2a2;
-    n2a2->g = n2a3;
-    n2a3->g = n2a4;
-    n2a4->g = n2a5;
-    n2a5->g = n2a1;
-    std::shared_ptr<Node2> b;
-    std::stringstream ss;
-    ss << n2a1;
-    ss >> b;
-
-    std::cout << n2a1 << "\n";
-    std::cout << b << "\n";
-
-    if (n2a1->g->g->g->g->g != n2a1) {
-      throw "not circular";
-    }
-  }
+  REQUIRE(b2.a_ea().get_pa() == 99);
+  REQUIRE(b2.a_ea().get_pb() == 99);
+  REQUIRE(b2.a_ep()->get_pa() == 1);
+  REQUIRE(b2.a_ep()->get_pb() == 2);
 }
+
+//   {
+//     // b1
+//     Base1_rev b1;
+//     b1.a_ea().set_pa(99);
+//     b1.a_ea().set_pb(99);
+//     std::cout << b1 << std::endl;
+
+//     b1.set_ep(&b1.a_ea());
+//     std::cout << b1 << std::endl;
+
+//     Elem e;
+//     b1.set_ep(&e);
+//     std::cout << b1 << "\n";
+//   }
+
+//   {
+//     // a* - a
+//     pNode pa;
+//     Node a;
+//     pa = &a;
+//     std::cout << a << "\n";
+//     std::cout << pa << "\n";
+//     std::cout << &pa << "\n";
+//   }
+
+//   {
+//     //       - b -
+//     // a - <       > - d
+//     //       - c -
+//     Node a, b, c, d;
+//     a.set_g(&b);
+//     a.set_d(&c);
+//     b.set_d(&d);
+//     c.set_g(&d);
+//     std::cout << a << "\n";
+//     std::cout << b << "\n";
+//     std::cout << c << "\n";
+//     std::cout << d << "\n";
+
+//     std::cout << &a << "\n";
+//     std::cout << &b << "\n";
+//     std::cout << &c << "\n";
+//     std::cout << &d << "\n";
+
+//     Node n;
+//     std::stringstream ss;
+//     ss << a;
+//     ss >> n;
+
+//     std::cout << "____"
+//               << "\n";
+//     std::cout << a << "\n";
+//     std::cout << " == "
+//               << "\n";
+//     std::cout << n << "\n";
+//     std::cout << "____"
+//               << "\n";
+
+//     if (n.a_d() == NULL) {
+//       std::cerr << "Error 1";
+//       throw "Error 1";
+//     }
+
+//     if (n.a_g() == NULL) {
+//       std::cerr << "Error 2";
+//       throw "Error 2";
+//     }
+
+//     if (n.a_g()->a_d() != n.a_d()->a_g()) {
+//       std::cerr << "Error 3";
+//       throw "Error 3";
+//     }
+//   }
+
+//   {
+//     Node a, b, c, d;
+//     Node a1, b1, c1, d1;
+
+//     a.a_g() = &b;
+//     b.a_g() = &c;
+//     c.a_g() = &d;
+//     d.a_g() = &a;
+
+//     std::cout << a << "\n";
+//     std::cout << b << "\n";
+//     std::cout << c << "\n";
+//     std::cout << d << "\n";
+
+//     std::stringstream ss;
+//     ss << a << b << c << d;
+//     ss >> a1 >> b1 >> c1 >> d1;
+//   }
+
+//   {
+//     psNode ps;
+//     if (ps != nullptr) {
+//       std::cerr << "Erreur 4";
+//       throw "erreur 5";
+//     }
+
+//     for (int i = 0; i < 100; ++i) {
+//       ps = std::make_shared<Node2>();
+//     }
+
+//     ps = nullptr;
+//   }
+
+//   // node point to the same
+//   {
+//     Node2 n2a;
+//     Node2 n2b;
+//     n2a.g = std::make_shared<Node2>();
+//     n2a.d = std::make_shared<Node2>();
+//     auto n = std::make_shared<Node2>();
+//     n2a.g->g = n;
+//     n2a.g->d = n;
+//     n2a.d->g = n;
+//     n2a.d->d = n;
+
+//     std::cout << n2a << "\n";
+//     std::cout << n2b << "\n";
+
+//     std::stringstream ss;
+//     ss << n2a;
+//     ss >> n2b;
+
+//     std::cout << n2a << "\n";
+//     std::cout << n2b << "\n";
+//   }
+
+//   // circular one
+//   {
+//     auto n2a1 = std::make_shared<Node2>();
+//     auto n2a2 = std::make_shared<Node2>();
+//     auto n2a3 = std::make_shared<Node2>();
+//     auto n2a4 = std::make_shared<Node2>();
+//     auto n2a5 = std::make_shared<Node2>();
+
+//     n2a1->g = n2a2;
+//     n2a2->g = n2a3;
+//     n2a3->g = n2a4;
+//     n2a4->g = n2a5;
+//     n2a5->g = n2a1;
+//     std::shared_ptr<Node2> b;
+//     std::stringstream ss;
+//     ss << n2a1;
+//     ss >> b;
+
+//     std::cout << n2a1 << "\n";
+//     std::cout << b << "\n";
+
+//     if (n2a1->g->g->g->g->g != n2a1) {
+//       throw "not circular";
+//     }
+//   }
+// }
