2a9b1c4d6e3ef2246eb93af0f6248fa0922a73ca
tiregram <ruhtra.mar@gmail.com>
2020-06-14 14:15:10 +0200

    add main to example

--- Examples/Car/src/main.cpp
+++ Examples/Car/src/main.cpp
@@ -1,9 +1,16 @@
+#include "Components/Car/Gates/controller.hpp"
 
-int main(int argc, char *argv[])
-{
-  
+int main(int argc, char *argv[]) {
+  Car::Gates::controller con;
+
+  con.configuration();
+  con.connection();
+  con.start();
+  for (int i = 0; i < 10;  ++i) {
+    con.step();
+  }
+
+  con.stop();
+
   return 0;
 }
-
-
-
