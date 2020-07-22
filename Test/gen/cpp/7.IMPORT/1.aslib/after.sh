d9f11e9aec54a37f579720092a03785f4051e690
tiregram <ruhtra.mar@gmail.com>
2020-07-22 10:57:08 +0200

    hot fix

--- Test/gen/cpp/7.IMPORT/1.aslib/after.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir ${COMPOME_PATH}/CompoMe/lib -p
-mkdir ${COMPOME_PATH}/CompoMe/inc -p
-mkdir ${COMPOME_PATH}/CompoMe/model -p
-
-cp inc/* ${COMPOME_PATH}/CompoMe/inc/. -r
-cp *.so *.a ${COMPOME_PATH}/CompoMe/lib/.
-cp *.yaml ${COMPOME_PATH}/CompoMe/
--- Test/gen/cpp/7.IMPORT/1.aslib/before.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export COMPOME_MODEL_PATH=.
-rm inc/catch.hpp src/catch.cpp
-touch dep1
-chmod +x dep1
