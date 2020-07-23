9a89add49608ea839f4f08dd8f4a4bad26434b74
tiregram <ruhtra.mar@gmail.com>
2020-07-23 14:43:14 +0200

    detect

--- Test/gen/cpp/6.TEST/1.cmd/test.sh
+++ Test/gen/cpp/6.TEST/1.cmd/test.sh
@@ -2,11 +2,9 @@
 
 set -euxo pipefail
 
-# cmake . >> cmake.log
-# make 2>&1  >> make.log
-# ./dep1 >> exec.log
 cmake .
 make --jobs=$[ $(nproc) ]
 cat order.cmd | ./dep1
 cat order2.cmd | ./dep2
+
 exit 0
