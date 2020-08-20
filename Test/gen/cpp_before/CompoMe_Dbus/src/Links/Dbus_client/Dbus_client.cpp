9c8f64e4fbde2ffb9999dfe39c306d9f07786d05
tiregram <ruhtra.mar@gmail.com>
2020-08-20 13:12:56 +0200

    repair example and mise en place de dbus client/server in the lib systems

---
+++ Test/gen/cpp_before/CompoMe_Dbus/src/Links/Dbus_client/Dbus_client.cpp
@@ -0,0 +1,148 @@
+#include "Links/Dbus_client/Dbus_client.hpp"
+
+#include "Interfaces/Function_dbus_send.hpp"
+#include "Interfaces/Interface.hpp"
+#include "Interfaces/Return_dbus_recv.hpp"
+
+class Return_dbus_recv_i : public CompoMe::Return_dbus_recv {
+  DBus::MessageIterator si;
+  bool r;
+
+public:
+  DBus::PendingCall::pointer p;
+  DBus::ReturnMessage::pointer rm;
+
+  void pull() override {
+    this->p->block();
+    auto rm_s = this->p->steal_reply();
+    this->rm = DBus::ReturnMessage::create(rm_s);
+    this->r = true;
+  }
+
+  void end() override {}
+
+  DBus::MessageIterator &get_si() override {
+    if (this->r) {
+      this->r = false;
+      this->si = this->rm->begin();
+    }
+    return si;
+  }
+};
+
+class Function_dbus_send_i : public CompoMe::Function_dbus_send {
+private:
+  DBus::CallMessage::pointer m;
+  DBus::Connection::pointer connection;
+  DBus::MessageAppendIterator ma;
+
+  bool reset;
+  Return_dbus_recv_i rs;
+
+  std::string dest;
+  std::string path;
+  std::string interface;
+
+public:
+  Return_dbus_recv_i &get_rs() { return rs; }
+
+  void set_interface(const std::string interface) {
+    this->interface = interface;
+  }
+
+  void set_path(const std::string path) { this->path = path; }
+
+  void set_dest(const std::string dest) { this->dest = dest; }
+
+  Function_dbus_send_i(DBus::Connection::pointer p_connection)
+      : connection(p_connection), rs() {}
+
+  virtual ~Function_dbus_send_i() {}
+
+  void set_function(std::string str) override { this->m->set_member(str); }
+
+  DBus::MessageAppendIterator &get_so() override {
+    if (this->reset) {
+      this->ma = m->append();
+      this->reset = false;
+    }
+
+    return ma;
+  }
+
+  void start() override {
+    m = DBus::CallMessage::create();
+    m->set_destination(this->dest);
+    m->set_path(this->path);
+    m->set_interface(this->interface);
+    this->reset = true;
+  }
+
+  void send() override {
+    this->rs.p = connection->send_with_reply_async(m, -1);
+  }
+};
+
+Dbus_client::Dbus_client() : CompoMe::Link() {}
+
+Dbus_client::~Dbus_client() {}
+
+void Dbus_client::step() { Link::step(); }
+
+void Dbus_client::connect() {
+  Link::connect();
+  if (!this->di) {
+    this->di = DBus::Dispatcher::create();
+    this->co = this->di->create_connection(DBus::BUS_SESSION);
+  }
+
+  for (auto &i_f : this->a_f) {
+    if (!i_f.first->connected()) {
+      this->connect(*i_f.first);
+    }
+  }
+}
+
+void Dbus_client::disconnect() {
+  Link::disconnect();
+  this->di = nullptr;
+  this->co = nullptr;
+  for (auto &i_f : this->function_dbus) {
+    i_f.first->disconnect_it();
+    free(i_f.second);
+    // this->function_dbus.erase(i_f);
+  }
+}
+void Dbus_client::connect(CompoMe::Require_helper &p_i) {
+  std::cout << "Connection ..."
+            << "\n";
+  auto data = this->a_f[&p_i];
+
+  Function_dbus_send_i *fs = new Function_dbus_send_i(this->co);
+  fs->set_dest(std::get<0>(data));
+  fs->set_path(std::get<1>(data));
+  fs->set_interface(std::get<2>(data));
+  p_i.fake_dbus_it(*fs, fs->get_rs());
+  this->function_dbus[&p_i] = fs;
+}
+
+void Dbus_client::disconnect(CompoMe::Require_helper &p_i) {
+  p_i.disconnect_it();
+  free(this->function_dbus[&p_i]);
+  this->function_dbus.erase(&p_i);
+}
+
+// Get and set /////////////////////////////////////////////////////////////
+
+CompoMe::String Dbus_client::get_object_name() const {
+  return this->object_name;
+}
+
+void Dbus_client::set_object_name(const CompoMe::String p_object_name) {
+  this->object_name = p_object_name;
+}
+i32 Dbus_client::get_timeout() const { return this->timeout; }
+
+void Dbus_client::set_timeout(const i32 p_timeout) {
+  this->timeout = p_timeout;
+}
---
+++ Test/gen/cpp_before/CompoMe_Dbus/src/Links/Dbus_server/Dbus_server.cpp
@@ -0,0 +1,166 @@
+#include "Links/Dbus_server/Dbus_server.hpp"
+
+#include "Interfaces/Interface.hpp"
+#include "Interfaces/Function_dbus_recv.hpp"
+#include "Interfaces/Return_dbus_send.hpp"
+
+class Function_dbus_recv_i : public CompoMe::Function_dbus_recv {
+
+  CompoMe::Serialization_context_import ctx;
+  DBus::CallMessage::pointer mc;
+  DBus::MessageIterator _it;
+  bool r;
+
+public:
+  Function_dbus_recv_i(DBus::CallMessage::pointer msg) : mc(msg), r(true) {}
+  void pull() override {}
+  void end() override {}
+
+  std::string get_function() override { return this->mc->member(); }
+
+  DBus::MessageIterator &get_si() override {
+    if (this->r == true) {
+      this->_it = this->mc->begin();
+      this->r = false;
+    }
+
+    return _it;
+  }
+
+  CompoMe::Serialization_context_import &get_ctx() override {
+    return this->ctx;
+  }
+
+  void reset() {
+    this->mc = DBus::CallMessage::create();
+    this->r = true;
+  }
+};
+
+class Return_dbus_send_i : public CompoMe::Return_dbus_send {
+  CompoMe::Serialization_context_export ctx;
+  DBus::ReturnMessage::pointer ret;
+  DBus::MessageAppendIterator _a_it;
+
+  bool r;
+
+public:
+  Return_dbus_send_i(DBus::ReturnMessage::pointer p_r) : ret(p_r), r(true) {}
+
+  void start() override {}
+  void send() override {}
+
+  DBus::MessageAppendIterator &get_so() override {
+    if (this->r) {
+      this->_a_it = this->ret->append();
+      this->r = false;
+    }
+
+    return _a_it;
+  }
+
+  CompoMe::Serialization_context_export &get_ctx() override { return ctx; }
+};
+
+Dbus_server::Dbus_server() : CompoMe::Link() {
+  static bool f = true;
+  if (f == true) {
+    DBus::init();
+    f = false;
+  }
+}
+
+Dbus_server::~Dbus_server() {}
+
+void Dbus_server::step() {
+  Link::step();
+  DBus::Message::pointer msg;
+
+  Link::step();
+
+  conn->read_write(0);
+  msg = conn->pop_message();
+
+  if (not msg) {
+    return;
+  }
+
+  if (msg->type() == DBus::CALL_MESSAGE) {
+    DBus::CallMessage::pointer msgc;
+    DBus::ReturnMessage::pointer reply;
+
+    msgc = DBus::CallMessage::create(msg);
+    reply = msgc->create_reply();
+
+    if (msgc->has_interface("org.freedesktop.DBus.Introspectable")) {
+      this->introspection(msgc, reply);
+    } else if (this->connected(msgc->path(), msgc->interface())) {
+      auto f_msg_r = Function_dbus_recv_i(msgc);
+      auto r_ret_s = Return_dbus_send_i(reply);
+      this->get_caller(msgc->path(), msgc->interface()).call(f_msg_r, r_ret_s);
+
+      std::cout << ">>" << reply->signature() << "<<\n";
+    } else {
+      std::cerr << "Not connected in the link..." << msgc->path() << ":"
+                << msgc->interface() << "." << msgc->member();
+    }
+
+    conn << reply;
+    conn->flush();
+  }
+}
+
+void Dbus_server::connect() {
+  Link::connect();
+  this->conn = DBus::Connection::create(DBus::BUS_SESSION, false);
+
+  // request a name on the bus
+  int ret = conn->request_name(this->get_object_name(),
+                               DBUS_NAME_FLAG_REPLACE_EXISTING);
+
+  if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) {
+    return;
+  }
+}
+
+void Dbus_server::disconnect() {
+  Link::disconnect();
+} // Get and set /////////////////////////////////////////////////////////////
+
+CompoMe::String Dbus_server::get_object_name() const {
+  return this->object_name;
+}
+
+void Dbus_server::set_object_name(const CompoMe::String p_object_name) {
+  this->object_name = p_object_name;
+}
+
+void Dbus_server::introspection(DBus::CallMessage::pointer msg,
+                                DBus::ReturnMessage::pointer reply) {
+
+  std::stringstream ss;
+  ss << "<node>";
+  ss << "<interface name=\"org.freedesktop.DBus.Introspectable\"><method "
+        "name=\"Introspect\"><arg type=\"s\" name=\"xml_data\" "
+        "direction=\"out\"/></method></interface>";
+  for (auto &kv2 : this->a_c[msg->path()]) {
+    ss << "\t<interface name=\"" << kv2.first << "\">";
+    kv2.second->introspection(ss);
+    ss << "</interface>\n";
+  }
+
+  std::cout << "path:" << msg->path() << "\n";
+  for (auto &kv : this->a_c) {
+    if (kv.first == msg->path()) {
+      continue;
+    }
+
+    int p = kv.first.find(msg->path());
+    if (p != std::string::npos) {
+      ss << "<node name=\"" << kv.first.substr(msg->path().length()) << "\"/>";
+    }
+  }
+  ss << "</node>";
+
+  reply << ss.str();
+}
