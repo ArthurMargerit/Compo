53a45b5756cbb7b8aff97e1b38226834d458db79
tiregram <ruhtra.mar@gmail.com>
2020-08-24 14:00:57 +0200

    cpp test

---
+++ Test/gen/cpp_before/Test/.compoMe.py
@@ -0,0 +1,23 @@
+import os
+
+
+if "COMPOME_PATH" not in os.environ:
+    print("Error: You have to define COMPOME_PATH")
+    exit(1)
+
+if "COMPOME_MODEL_PATH" not in os.environ:
+    print("Error: You have to define COMPOME_MODEL_PATH")
+    exit(1)
+
+COMPOME_PATH = os.environ["COMPOME_PATH"]
+COMPOME_MODEL_PATH = os.environ["COMPOME_MODEL_PATH"].split(":")
+PROJECT = os.path.basename(os.environ["PWD"])
+PROJECT_PATH = os.path.dirname(os.environ["PWD"])
+
+CONFIG = {
+    "migration": "simple",
+    "jinja_template_path": [os.environ["COMPOME_PATH"] + "/generate_model/cpp", "."],
+    "generation_model": os.environ["COMPOME_PATH"] + "/generate_model/cpp/generation.yaml",
+    "import_path":  COMPOME_MODEL_PATH,
+    "template_options": {"project": {"name": PROJECT, "path": PROJECT_PATH}}
+}
