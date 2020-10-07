aab3fc3f425ca4d5bd198711d96ce598a28c49b6
tiregram <ruhtra.mar@gmail.com>
2020-10-07 12:41:24 +0200

    bug

--- Test/gen/cpp/8.SWIG/4.Link/test.sh
+++ Test/gen/cpp/8.SWIG/4.Link/test.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash
+
 set -euxo pipefail
 
 cmake -DCMAKE_INSTALL_PREFIX=${COMPOME_PATH}/CompoMe .
@@ -7,7 +8,6 @@ make install
 
 p=$(pwd)
 cd ${COMPOME_PATH}/CompoMe
-export LD_LIBRARY_PATH=${COMPOME_PATH}/CompoMe/lib
 python3 ${p}/test.py
 cd $p
 
