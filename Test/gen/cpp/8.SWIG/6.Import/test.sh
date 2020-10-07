b641049a018d9f4da04ddebb8ef29a7b759d319b
tiregram <ruhtra.mar@gmail.com>
2020-10-07 14:10:17 +0200

    bug3

--- Test/gen/cpp/8.SWIG/6.Import/test.sh
+++ Test/gen/cpp/8.SWIG/6.Import/test.sh
@@ -1,14 +1,11 @@
 #!/bin/env bash
-
 set -euxo pipefail
 
 cmake -DCMAKE_INSTALL_PREFIX=${COMPOME_PATH}/CompoMe .
 make --jobs=$[ $(nproc) ]
 make install
 
-p=$(pwd)
-cd ${COMPOME_PATH}/CompoMe
-LD_LIBRARY_PATH=${COMPOME_PATH}/CompoMe/lib PYTHONPATH=${COMPOME_PATH}/CompoMe/ python3 ${p}/test.py
-cd $p
-
+export LD_LIBRARY_PATH=${COMPOME_PATH}/CompoMe/lib
+export PYTHONPATH=${COMPOME_PATH}/CompoMe
+python3 test.py
 exit 0
