d89fde23e58a29cc34a4431d21205f0e728a5575
tiregram <ruhtra.mar@gmail.com>
2020-12-18 17:35:25 +0100

    mise en place du build recursif du compome core

--- Test/gen/run_before.sh
+++ Test/gen/run_before.sh
@@ -1,28 +1,43 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+DEBUG_RELEASE=Debug # Release
+if [[ -v "COMPO_WORKDIR_TMP" && "$COMPO_WORKDIR_TMP" == 1 ]]
+then
+    COMPO_WORKDIR=$(mktemp -d)
+    COMPO_RMDIR=1
+else
+    COMPO_WORKDIR=tmp_before
+    COMPO_RMDIR=0
+    rm -rf tmp_before
+fi
 
 for target in $@ ; do
-    echo $(tput setab 4 ) ${target} $(tput sgr0)
-    DEBUG_RELEASE=Debug # Release
-    if [[ -v "COMPO_WORKDIR_TMP" && "$COMPO_WORKDIR_TMP" == 1 ]]
-    then
-        COMPO_WORKDIR=$(mktemp -d)
-        COMPO_RMDIR=1
-    else
-        COMPO_WORKDIR=tmp_before
-        COMPO_RMDIR=0
-        rm -rf tmp_before
-    fi
+    echo $(tput setab 4 ) cp ${target} $(tput sgr0)
 
     mkdir -p ${COMPO_WORKDIR}
     cp -r ${target} ${COMPO_WORKDIR}
+    export COMPOME_MODEL_PATH=$(realpath ${COMPO_WORKDIR}/$(basename ${target})):${COMPOME_MODEL_PATH}
+done
+
+echo $COMPOME_MODEL_PATH
+# echo $(realpath $@ | tr '\n' ':' )
+# export COMPOME_MODEL_PATH=$(realpath $@ | tr '\n' ':' )${COMPOME_MODEL_PATH}
+
+for target in $@ ; do
+    echo $(tput setab 4 ) gen ${target} $(tput sgr0)
     cd ${COMPO_WORKDIR}/$(basename ${target})
     ${COMPOME_PATH}/compome generate -f *.yaml
+    cd ../..
+done
+
+for target in $@ ; do
+    cd ${COMPO_WORKDIR}/$(basename ${target})
+    echo $(tput setab 4 ) build ${target} $(tput sgr0)
 
     if [ "$COMPOME_MODEL" == "CPP" ]
     then
-        cmake -DCMAKE_BUILD_TYPE=${DEBUG_RELEASE} -DCMAKE_INSTALL_PREFIX=${COMPOME_PATH}/CompoMe .
+        cmake -DCMAKE_BUILD_TYPE=${DEBUG_RELEASE} -DCMAKE_INSTALL_PREFIX=${COMPOME_PATH}/build/${target} .
         make -j8
         make install
     elif [ "$COMPOME_MODEL" == "GRAPH" ]
@@ -36,9 +51,10 @@ for target in $@ ; do
         done
     fi
 
-    cd -
-    if [[ $COMPO_RMDIR == 1 ]]
-    then
-        rm $COMPO_WORKDIR
-    fi
+    cd ../..
 done
+
+if [[ $COMPO_RMDIR == 1 ]]
+then
+    rm -r $COMPO_WORKDIR
+fi
