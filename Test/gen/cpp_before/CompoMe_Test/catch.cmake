a8bb23633586b6c64908fda106ac4867eade0d73
tiregram <ruhtra.mar@gmail.com>
2021-01-06 17:48:23 +0100

    cpp

--- Test/gen/cpp_before/CompoMe_Test/catch.cmake
+++ Test/gen/cpp_before/CompoMe_Test/catch.cmake
@@ -1,24 +1,8 @@
-cmake_minimum_required(VERSION 3.0)
-project("compo_catch")
-set(CMAKE_CXX_STANDARD 11)
 
-add_library(Catch_thread_static STATIC src/catch_thread.cpp)
-add_library(Catch_static STATIC src/catch.cpp)
-#add_library(Catch_shared SHARED src/catch.cpp)
-#add_library(Catch_module MODULE src/catch.cpp)
+add_library(Catch_static STATIC src/catch.cpp )
+target_include_directories(Catch_static PUBLIC inc)
 
+add_library(Catch_thread_static STATIC src/catch_thread.cpp )
+target_include_directories(Catch_thread_static PUBLIC inc)
 
-target_include_directories(Catch_static PUBLIC "inc")
-target_include_directories(Catch_thread_static PUBLIC "inc")
-#target_include_directories(Catch_shared PUBLIC "inc")
-#target_include_directories(Catch_module PUBLIC "inc")
-
-install(TARGETS Catch_static Catch_thread_static #Catch_shared Catch_module
-        LIBRARY DESTINATION lib
-        ARCHIVE DESTINATION lib)
-
-install(DIRECTORY inc/ DESTINATION inc
-        FILES_MATCHING PATTERN "*.hpp")
-
-install(FILES CompoMe_Test.yaml DESTINATION .
-)
+install(TARGETS Catch_static Catch_thread_static)
