2093665de8dfb9eca6d8b168cb053ae85d195797
ruhtra <ruhtra.mar@gmail.com>
2021-01-19 00:27:08 +0100

    clean enum

--- Test/gen/cpp/0.EMPTY/2.tools/src/main.cpp
+++ Test/gen/cpp/0.EMPTY/2.tools/src/main.cpp
@@ -5,37 +5,37 @@ TEST_CASE("CompoMe config componentx", "[Config]" ) {
   CompoMe::Config::DataProvider d;
 
   auto& a = d.get_access();
-  REQUIRE(a.add("key1","A") == CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.add("key2","B") == CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.add("key3","C") == CompoMe::Config::RESULT_STATE_VALID);
+  REQUIRE(a.add("key1","A") == CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.add("key2","B") == CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.add("key3","C") == CompoMe::Config::Result_state::VALID);
 
   REQUIRE(a.exist("key1") == true);
   REQUIRE(a.exist("key2") == true);
   REQUIRE(a.exist("key3") == true);
 
-  REQUIRE(a.get("key1").a_state() == CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.get("key2").a_state() == CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.get("key3").a_state() == CompoMe::Config::RESULT_STATE_VALID);
+  REQUIRE(a.get("key1").a_state() == CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.get("key2").a_state() == CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.get("key3").a_state() == CompoMe::Config::Result_state::VALID);
 
   REQUIRE(a.get("key1").a_result() == "A");
   REQUIRE(a.get("key2").a_result() == "B");
   REQUIRE(a.get("key3").a_result() == "C");
 
-  REQUIRE(a.set("key1", "A1") == CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.set("key2", "B1") == CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.set("key3", "C1") == CompoMe::Config::RESULT_STATE_VALID);
+  REQUIRE(a.set("key1", "A1") == CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.set("key2", "B1") == CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.set("key3", "C1") == CompoMe::Config::Result_state::VALID);
 
-  REQUIRE(a.get("key1").a_state() == CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.get("key2").a_state() == CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.get("key3").a_state() == CompoMe::Config::RESULT_STATE_VALID);
+  REQUIRE(a.get("key1").a_state() == CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.get("key2").a_state() == CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.get("key3").a_state() == CompoMe::Config::Result_state::VALID);
 
   REQUIRE(a.get("key1").a_result() == "A1");
   REQUIRE(a.get("key2").a_result() == "B1");
   REQUIRE(a.get("key3").a_result() == "C1");
 
-  REQUIRE(a.del("key1")== CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.del("key2")== CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.del("key3")== CompoMe::Config::RESULT_STATE_VALID);
+  REQUIRE(a.del("key1")== CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.del("key2")== CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.del("key3")== CompoMe::Config::Result_state::VALID);
 
   REQUIRE(a.exist("key1") == false);
   REQUIRE(a.exist("key2") == false);
@@ -48,9 +48,9 @@ TEST_CASE("config data exist/add/del", "[Config]" ) {
   auto& a = d.get_access();
 
   REQUIRE(a.exist("key") == false);
-  REQUIRE(a.add("key","1") == CompoMe::Config::RESULT_STATE_VALID);
+  REQUIRE(a.add("key","1") == CompoMe::Config::Result_state::VALID);
   REQUIRE(a.exist("key") == true);
-  REQUIRE(a.del("key") == CompoMe::Config::RESULT_STATE_VALID);
+  REQUIRE(a.del("key") == CompoMe::Config::Result_state::VALID);
   REQUIRE(a.exist("key") == false);
 }
 
@@ -58,14 +58,14 @@ TEST_CASE("config data exist/add/del", "[Config]" ) {
 TEST_CASE("config data double add", "[Config]" ) {
   CompoMe::Config::DataProvider d;
   auto& a = d.get_access();
-  REQUIRE(a.add("key","1") == CompoMe::Config::RESULT_STATE_VALID);
-  REQUIRE(a.add("key","2") == CompoMe::Config::RESULT_STATE_INVALID_KEY);
+  REQUIRE(a.add("key","1") == CompoMe::Config::Result_state::VALID);
+  REQUIRE(a.add("key","2") == CompoMe::Config::Result_state::INVALID_KEY);
 }
 
 TEST_CASE("config unex key", "[Config]" ) {
   CompoMe::Config::DataProvider d;
   auto& a = d.get_access();
-  REQUIRE(a.del("key") == CompoMe::Config::RESULT_STATE_INVALID_KEY);
-  REQUIRE(a.set("key","1") == CompoMe::Config::RESULT_STATE_INVALID_KEY);
-  REQUIRE(a.get("key").a_state() == CompoMe::Config::RESULT_STATE_INVALID_KEY);
+  REQUIRE(a.del("key") == CompoMe::Config::Result_state::INVALID_KEY);
+  REQUIRE(a.set("key","1") == CompoMe::Config::Result_state::INVALID_KEY);
+  REQUIRE(a.get("key").a_state() == CompoMe::Config::Result_state::INVALID_KEY);
 }
