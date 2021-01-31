01d4d51319028f5294d0c296d97d9e65e273d2af
ruhtra <ruhtra.mar@gmail.com>
2021-01-31 14:44:31 +0100

    clean 7

---
+++ Test/gen/core_build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export COMPOME_PATH=$(realpath ../../)
+export COMPOME_MODEL=ALL
+
+rm ${COMPOME_PATH}/build/* -rf
+
+echo "__CORE__"
+./run_before.sh cpp_before/CompoMe cpp_before/CompoMe_Async cpp_before/CompoMe_Log cpp_before/CompoMe_Swig cpp_before/CompoMe_Test cpp_before/CompoMe_Tools
+
+echo "__CORE__OPT"
+./run_before.sh cpp_before/CompoMe_Dbus cpp_before/CompoMe_Http cpp_before/CompoMe_Https cpp_before/CompoMe_Tcp cpp_before/CompoMe_Udp cpp_before/CompoMe_Zmq
