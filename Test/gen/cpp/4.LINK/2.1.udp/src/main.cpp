b36364abfb9b36388b4a4c7a65ceae2b41a0e436
ArthurMargerit <arthur.margerit.ext@exxact-robotics.com>
2021-07-14 14:20:58 +0200

    update add port to udp

--- Test/gen/cpp/4.LINK/2.1.udp/src/main.cpp
+++ Test/gen/cpp/4.LINK/2.1.udp/src/main.cpp
@@ -1,4 +1,5 @@
 #include "Components/C_p.hpp"
+
 #include "Links/CompoMe/Posix/Udp_client_out/Udp_client_out.hpp"
 #include "Links/CompoMe/Posix/Udp_server_in/Udp_server_in.hpp"
 #include "catch.hpp"
@@ -14,17 +15,16 @@ std::mutex mylockapp;
 static std::mutex l;
 
 /// client 1
-void client(CompoMe::String c = "", CompoMe::String i = "") {
+void client() {
   CompoMe::Posix::Udp_client_out client;
   // S_udp_client client;
   client.set_addr("127.0.0.1");
   client.set_port(8080);
-  client.set_to_component(c);
-  client.set_to_interface(i);
 
   CompoMe::Require_helper_t<I1> req;
-  client.set_out(req);
-  client.connect();
+  client.get_main().connect_require(req);
+
+  client.main_connect();
 
   for (int i = 0; i < 10000; i++) {
     req->f1();
@@ -32,7 +32,8 @@ void client(CompoMe::String c = "", CompoMe::String i = "") {
     REQUIRE_LOCK(req->f3(i) == i + 1);
     REQUIRE_LOCK(req->f4(i, i * 2) == i + i * 2 + 1);
   }
-  client.disconnect();
+
+  client.main_disconnect();
 }
 
 TEST_CASE("Link simple", "[Link][simple]") {
@@ -41,11 +42,11 @@ TEST_CASE("Link simple", "[Link][simple]") {
 
   C_p p;
   CompoMe::Posix::Udp_server_in server;
-  //  S_udp_server server;
+
   server.set_addr("127.0.0.1");
   server.set_port(8080);
-  server.set_in(&p.get_ii());
-  server.connect();
+  server.get_main().connect_interface(p.get_ii());
+  server.main_connect();
 
   std::thread t2([&server, &lock]() {
     while (!lock.try_lock()) {
@@ -82,41 +83,5 @@ TEST_CASE("Link simple", "[Link][simple]") {
   lock.unlock();
   t2.join();
 
-  server.disconnect();
-}
-
-TEST_CASE("Link multi", "[Link][multi_client]") {
-  std::mutex lock;
-  lock.lock();
-
-  C_p p;
-  CompoMe::Posix::Udp_server_in server;
-  server.set_addr("127.0.0.1");
-  server.set_port(8080);
-  server.set_in(&p.get_ii());
-  server.connect();
-
-  std::thread t2([&server, &lock]() {
-    while (!lock.try_lock()) {
-      server.step();
-    }
-  });
-
-  // client 1
-  std::thread tc_1([]() { client(); });
-  std::thread tc_2([]() { client(); });
-  std::thread tc_3([]() { client(); });
-  std::thread tc_4([]() { client(); });
-  std::thread tc_5([]() { client(); });
-
-  tc_1.join();
-  tc_2.join();
-  tc_3.join();
-  tc_4.join();
-  tc_5.join();
-
-  lock.unlock();
-  t2.join();
-
-  server.disconnect();
+  server.main_disconnect();
 }
