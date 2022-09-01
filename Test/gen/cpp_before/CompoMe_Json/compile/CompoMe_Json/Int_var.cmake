5312d522b59138e30c4d7433ec2c3c103a9e66cc
Arthur Margerit <arthur.margerit@exxact-robotics.com>
2022-09-01 17:59:32 +0200

    add compome json as int

--- Test/gen/cpp_before/CompoMe_Json/compile/CompoMe_Json/Int_var.cmake
+++ Test/gen/cpp_before/CompoMe_Json/compile/CompoMe_Json/Int_var.cmake
@@ -1,2 +1,3 @@
 LIST(APPEND src_file "src/CompoMe/Json/Call.cpp")
+LIST(APPEND src_file "src/json.cpp")
 LIST(APPEND inc_file "inc/CompoMe/json/Call.hpp")
--- Test/gen/cpp_before/CompoMe_Json/src/json.cpp
+++ Test/gen/cpp_before/CompoMe_Json/src/json.cpp
@@ -1,1 +1,19 @@
-#include "json.hpp"
+#include "Types/CompoMe/Json.hpp"
+#include "Interfaces/Function_stream_recv.hpp"
+
+namespace CompoMe {
+
+// function to allow int and double parsing
+CompoMe::Function_stream_recv &operator>>(CompoMe::Function_stream_recv &s,
+                                          nlohmann::json &e) {
+  char r = s.get_si().peek();
+  if (r == '[' || r == 'f' || r == 't' || r == '{' || r == '"' || r == '\'') {
+    s.get_si() >> e;
+  } else {
+    double a;
+    s.get_si() >> a;
+    e = a;
+  }
+  return s;
+}
+} // namespace CompoMe
